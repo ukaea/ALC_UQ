1ba1560e7940410feffd80b1f9b0742ba45dda2a
Buchanan, James <james.buchanan@ukaea.uk>
2021-01-28 10:57:34 +0000

    Removed unintended changes

--- user_interface/python/interface2.py
+++ user_interface/python/interface2.py
@@ -1,7 +1,6 @@
 # Finalises the iteration by setting the failure flag to 0
 
 import dakota.interfacing as di
-import os
 
 # Set a default value for the iteration number
 iteration = -1
@@ -13,14 +12,6 @@ try:
 
     # Get Iteration number
     iteration = params.eval_num
-
-    # Wait for dummy finished file to exist
-    while( True ):
-
-        if os.exists('FINISHED.TXT'):
-            break
-        else:
-            time.sleep(20)
 
     # Return response indicating run was successful
     results['Failed'].function = 0
